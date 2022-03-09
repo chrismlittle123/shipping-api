208f9270574b2b7f0bf42dabdaf5a2e3a0238650
chrislittlecazoo <chris.little@cazoo.co.uk>
2022-03-09 17:49:36 +0000

    Change sample_lambda to data_ingestion 4

--- src/data_ingestion/handler.py
+++ src/data_ingestion/handler.py
@@ -1,13 +1,14 @@
 import json
 
 import requests
+from aws_lambda_powertools.utilities.typing import LambdaContext
 
 
 def get_urls() -> dict:
     return {"cinch": "https://www.cinch.co.uk/"}
 
 
-def handler(event, context):
+def handler(event: dict, context: LambdaContext) -> dict:
     urls = get_urls()
     sample_url = urls["cinch"]
     resp = requests.get(sample_url)
