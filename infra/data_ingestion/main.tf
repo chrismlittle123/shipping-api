79ef2c6dc106f3e0735011703b0cf37e741d2ec1
chrislittlecazoo <chris.little@cazoo.co.uk>
2022-03-11 14:27:02 +0000

    Add dead letter queue to lambda 4

--- infra/data_ingestion/main.tf
+++ infra/data_ingestion/main.tf
@@ -2,11 +2,11 @@ resource "aws_iam_role" "lambda_iam" {
   force_detach_policies = true
   name                  = "${var.project}-lambda-role"
   assume_role_policy    = data.aws_iam_policy_document.assume_role_lambda.json
-  path                  = "/${var.project}/${local.service_name}/"
+  path                  = "/${var.project}/${var.service_name}/"
 }
 
 resource "aws_iam_policy" "inline_policy" {
-  name   = "${var.project}-${local.service_name}-policy"
+  name   = "${var.project}-${var.service_name}-policy"
   policy = data.aws_iam_policy_document.lambda_policy.json
 }
 
