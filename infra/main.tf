51420b6438928199d0f0b7daec6fc57add8d7902
chrislittlecazoo <chris.little@cazoo.co.uk>
2022-03-11 14:52:05 +0000

    Add dead letter queue to lambda 8

--- infra/main.tf
+++ infra/main.tf
@@ -20,5 +20,6 @@ module "data-ingestion" {
   aws_account_id = var.aws_account_id
   aws_region     = var.aws_region
   project        = var.project
+  service_name   = var.service_name
   source         = "./data_ingestion"
 }
