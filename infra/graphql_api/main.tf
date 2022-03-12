df57427b1fd2c00862a5e983c19b4de2ce177ec9
chrislittlecazoo <chris.little@cazoo.co.uk>
2022-03-12 00:36:16 +0000

    Add example graphql schema 12

--- infra/graphql_api/main.tf
+++ infra/graphql_api/main.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "shipping_datasource_role" {
 
 resource "aws_appsync_datasource" "shipping_data" {
   api_id           = aws_appsync_api_key.appsync_api_key.api_id
-  name             = "shipping-data"
+  name             = "shipping_data"
   service_role_arn = aws_iam_role.shipping_datasource_role.arn
   type             = "AMAZON_DYNAMODB"
 
