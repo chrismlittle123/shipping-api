2cbd9e77b6cbab3f68d5fc9353a377346889a414
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-12 09:25:36 +0000

    Change lambda IAM role names

--- infra/rest_api/main.tf
+++ infra/rest_api/main.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role" "lambda_iam" {
   force_detach_policies = true
-  name                  = "${var.project}-lambda-role"
+  name                  = "${var.service_name}-lambda-role"
   assume_role_policy    = data.aws_iam_policy_document.assume_role_lambda.json
   path                  = "/${var.project}/${var.service_name}/"
 }
