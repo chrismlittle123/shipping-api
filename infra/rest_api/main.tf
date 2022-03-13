aa0b366db76dedfdb995617eedde58c6c3e639fa
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-13 09:00:50 +0000

    Integrate AWS Lambda with API Gateway 2

--- infra/rest_api/main.tf
+++ infra/rest_api/main.tf
@@ -19,30 +19,29 @@ resource "aws_iam_role_policy_attachment" "lambda_iam_to_policy_attachment" {
 
 # API Gateway Rest API
 
-resource "aws_api_gateway_rest_api" "rest_api" {
+resource "aws_api_gateway_rest_api" "rest_api_name" {
   name        = "${var.project}-${var.service_name}"
   description = "API which connects to shipping-data DynamoDB table"
 
 }
 
-resource "aws_api_gateway_resource" "rest_api" {
-  parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.rest_api.id
+resource "aws_api_gateway_resource" "rest_api_resource" {
+  parent_id   = aws_api_gateway_rest_api.rest_api_name.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.rest_api_name.id
   path_part   = "vessels"
 }
 
-resource "aws_api_gateway_method" "rest_api" {
-  resource_id   = aws_api_gateway_resource.rest_api.id
-  rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+resource "aws_api_gateway_method" "rest_api_method" {
+  rest_api_id   = aws_api_gateway_rest_api.rest_api_name.id
+  resource_id   = aws_api_gateway_resource.rest_api_resource.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "integration" {
-  rest_api_id             = aws_api_gateway_rest_api.api.id
-  resource_id             = aws_api_gateway_resource.resource.id
-  http_method             = aws_api_gateway_method.method.http_method
-  uri                     = aws_lambda_function.lambda.invoke_arn
+  rest_api_id             = aws_api_gateway_rest_api.rest_api_name.id
+  resource_id             = aws_api_gateway_resource.rest_api_resource.id
+  http_method             = aws_api_gateway_method.rest_api_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
 }
