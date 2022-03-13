23196ab34fa2726348057c40d8c6638f3e0a5c5a
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-13 12:12:01 +0000

    Refactor rest_api handler

--- src/rest_api/handler.py
+++ src/rest_api/handler.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from typing import Any, Dict
 
 from aws_lambda_powertools.utilities.typing import LambdaContext
 
@@ -14,15 +15,15 @@ def handler(event: dict, context: LambdaContext) -> dict:
     transactionId = event["query"]["transactionId"]
 
     # 2. Construct the body of the response object
-    transactionResponse = dict()
+    transactionResponse: Dict[Any, Any] = dict()
     transactionResponse["transactionId"] = transactionId
     transactionResponse["message"] = "Hello from Lambda land"
 
     # 3. Construct http response object
-    responseObject = dict()
-    responseObject["statusCode"] = "200"
-    responseObject["headers"] = json.dumps({"Content-Type": "application/json"})
-    responseObject["body"] = json.dumps(transactionResponse)
+    responseObject: Dict[Any, Any] = dict()
+    responseObject["statusCode"] = 200
+    responseObject["headers"] = {"Content-Type": "application/json"}
+    responseObject["body"] = transactionResponse
 
     # 4. Return the response object
     return responseObject
