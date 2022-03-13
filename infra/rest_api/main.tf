2d1cb48045765a32b4f1402156be2f1e16741dc4
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-13 09:03:12 +0000

    Integrate AWS Lambda with API Gateway 2

--- infra/rest_api/main.tf
+++ infra/rest_api/main.tf
@@ -46,17 +46,6 @@ resource "aws_api_gateway_integration" "integration" {
   type                    = "AWS_PROXY"
 }
 
-# Lambda
-resource "aws_lambda_permission" "apigw_lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = "${var.project}-dev-${var.service_name}"
-  principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.rest_api.path}"
-}
-
 # Dead Letter Queues
 
 resource "aws_sns_topic" "dead_letter_topic" {
