ac18c74eade5bd31a2881cb232d108350ae63698
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-13 11:48:16 +0000

    Add URI to AWS lambda integration resource

--- infra/rest_api/main.tf
+++ infra/rest_api/main.tf
@@ -44,6 +44,7 @@ resource "aws_api_gateway_integration" "integration" {
   http_method             = aws_api_gateway_method.rest_api_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
+  uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/arn:aws:lambda:${var.aws_region}:${var.aws_account_id}:function:${var.project}-${var.service_name}/invocations"
 }
 
 resource "aws_api_gateway_deployment" "rest_api_deployment" {
