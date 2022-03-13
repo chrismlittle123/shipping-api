d699a1d0862071fb03bcceb0f36fff2098fbe187
chrismlittle123 <chrismlittle123@gmail.com>
2022-03-13 08:53:57 +0000

    Integrate AWS Lambda with API Gateway

--- infra/rest_api/main.tf
+++ infra/rest_api/main.tf
@@ -20,23 +20,43 @@ resource "aws_iam_role_policy_attachment" "lambda_iam_to_policy_attachment" {
 # API Gateway Rest API
 
 resource "aws_api_gateway_rest_api" "rest_api" {
-  name = "${var.project}-${var.service_name}"
+  name        = "${var.project}-${var.service_name}"
+  description = "API which connects to shipping-data DynamoDB table"
 
 }
 
 resource "aws_api_gateway_resource" "rest_api" {
   parent_id   = aws_api_gateway_rest_api.rest_api.root_resource_id
-  path_part   = "vessels"
   rest_api_id = aws_api_gateway_rest_api.rest_api.id
+  path_part   = "vessels"
 }
 
 resource "aws_api_gateway_method" "rest_api" {
-  authorization = "NONE"
-  http_method   = "GET"
   resource_id   = aws_api_gateway_resource.rest_api.id
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
+  http_method   = "GET"
+  authorization = "NONE"
 }
 
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.resource.id
+  http_method             = aws_api_gateway_method.method.http_method
+  uri                     = aws_lambda_function.lambda.invoke_arn
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+}
+
+# Lambda
+resource "aws_lambda_permission" "apigw_lambda" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = "${var.project}-dev-${var.service_name}"
+  principal     = "apigateway.amazonaws.com"
+
+  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+  source_arn = "arn:aws:execute-api:${var.aws_region}:${var.aws_account_id}:${aws_api_gateway_rest_api.api.id}/*/${aws_api_gateway_method.method.http_method}${aws_api_gateway_resource.rest_api.path}"
+}
 
 # Dead Letter Queues
 
